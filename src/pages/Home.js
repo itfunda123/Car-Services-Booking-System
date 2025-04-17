2ea0854aaf7655e4e7c5511c470544a5456a4aec
unknown <melkywafula29@gmail.com>
2025-04-17 15:37:51 +0300

    Frontend Updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import image1 from '../Assets/image1.jpg';
-import image2 from '../Assets/image2.jpg';
-import image3 from '../Assets/image3.jpg';
+import image2 from '../Assets/image1.jpg';
+import image3 from '../Assets/image1.jpg';
+import logo1 from '../Assets/audi.jpeg'; // Import the car logo
+import logo2 from '../Assets/Bmw.jpeg';
+import logo3 from '../Assets/vw.png';
+import logo4 from '../Assets/subaru.jpeg'; // Import the car logo
+import logo5 from '../Assets/toyota.jpeg';
+import logo6 from '../Assets/tesla.jpeg' // Import the car logo
+import logo7 from '../Assets/peugeot.png'; // Import the car logo
+import logo8 from '../Assets/mazda.jpeg'; // Import the car logo
+import logo9 from '../Assets/ford.jpeg'; // Import the car logo
+import logo10 from '../Assets/ferrari.jpeg'; // Import the car logo
 
 const Home = () => {
   return (
@@ -51,6 +61,20 @@ const Home = () => {
       <div className="mt-4 text-center">
         <h2>Welcome to Our Car Service Booking Platform</h2>
         <p>Book your car service appointment quickly and easily!</p>
+      </div>
+
+      {/* Car logos section - Same logo for all images */}
+      <div className="d-flex justify-content-center mt-4">
+        <img src={logo1} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo2} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo3} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo4} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo5} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo6} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo7} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo8} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo9} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
+        <img src={logo10} alt="Car Logo" style={{ width: '60px', margin: '0 10px' }} />
       </div>
     </div>
   );
